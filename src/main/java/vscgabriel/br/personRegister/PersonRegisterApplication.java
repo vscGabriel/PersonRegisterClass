b0d10a0f3e7b0b0bd0d8b5c7e9f29e3e9b66f06b
Gabriel Cardoso <gvscceca@gmail.com>
2024-04-19 19:35:25 -0300

    init commit

---
+++ src/main/java/vscgabriel/br/personRegister/PersonRegisterApplication.java
@@ -0,0 +1,13 @@
+package vscgabriel.br.personRegister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vscgabriel/br/personRegister/PersonRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package vscgabriel.br.personRegister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
